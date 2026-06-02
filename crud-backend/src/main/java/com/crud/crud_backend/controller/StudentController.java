1733dd1a1476d70d1a56e989f7e102ab88e8e113
Saaiprasath33 <saaiprasath.s2024aids@sece.ac.in>
2026-06-02 12:13:48 +0530

    initial

--- crud-backend/src/main/java/com/crud/crud_backend/controller/StudentController.java
+++ crud-backend/src/main/java/com/crud/crud_backend/controller/StudentController.java
@@ -1,35 +1,52 @@
-package com.crud.crud_backend.Controller;
+package com.crud.crud_backend.controller;
+
 import com.crud.crud_backend.model.Student;
 import com.crud.crud_backend.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
 @RestController
 @RequestMapping("/students")
 public class StudentController {
+
     @Autowired
     private StudentService s;
+
     @GetMapping
     public List<Student> getAllStudents() {
         return s.getAllStudentData();
     }
+
     @PostMapping
     public String addStudent(@RequestBody Student std) {
         s.addStudent(std);
         return "Successfully added to the database";
     }
+
     @GetMapping("/{rno}")
     public Student getStudentByRollNo(@PathVariable int rno) {
         return s.getStudentByRollNo(rno);
     }
+
     @PutMapping
     public String updateStudent(@RequestBody Student std) {
         s.updateStudent(std);
         return "Successfully updated";
     }
+
     @DeleteMapping("/{rno}")
     public String deleteStudent(@PathVariable int rno) {
         s.deleteStudent(rno);
         return "Successfully deleted";
+    }
+
+    @GetMapping("/custom")
+    public List<Student> getStudentByGenderAndTech(
+            @RequestParam String gender,
+            @RequestParam String tech) {
+
+        return s.getStudentByGenderAndTech(gender, tech);
     }
 }
--- crud-backend/src/main/java/com/crud/crud_backend/repository/StudentRepo.java
+++ crud-backend/src/main/java/com/crud/crud_backend/repository/StudentRepo.java
@@ -4,7 +4,11 @@ import com.crud.crud_backend.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+    List<Student> findByGenderAndTech(String gender, String tech);
 
 }
